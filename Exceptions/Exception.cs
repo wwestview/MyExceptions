6afa1342d74480c92629ff110a0cf4f15cc7c7e1
Nazar Kolachko <biokapt@gmail.com>
2024-11-29 12:16:02 +0200

    Fixed some bags

--- Exceptions/Exception.cs
+++ Exceptions/Exception.cs
@@ -2,6 +2,8 @@
 using System.Drawing.Text;
 using System.Text.RegularExpressions;
 using System.Drawing;
+using System.Net;
+using System.Drawing.Imaging;
 class Exceptions
 {
     static void FileProcessing()
@@ -12,50 +14,37 @@ class Exceptions
         int count = 0;
         var n = new Random();
         var m = new Random();
-        
-      
         File.Create("no_file.txt").Close();
         File.Create("overflow.txt").Close();
         File.Create("bad_data.txt").Close();
+        // 10 - bad_data; 15 - bad_data; 16 - bad_data; 19 - no_file; 29 - overflow;
 
         for (int i = 10; i <= 29; i++)
         {
-            using (sw = new($"{i}.txt"))
+           /* using (sw = new($"{i}.txt"))
             {
                 sw.WriteLine(n.Next(1,50));
                 sw.WriteLine(m.Next(1,50));
             }
-           
-
+           */
+               
             try
             {
                 sr = new($"{i}.txt");
-                try
+                int a = int.Parse(sr.ReadLine());
+                int b = int.Parse(sr.ReadLine());
+                checked
                 {
-                    int a = int.Parse(sr.ReadLine());
-                    int b = int.Parse(sr.ReadLine());
-                    try
-                    {
-                        checked
-                        {
-                            int ab = a * b;
-                            sum += ab;
-                            count++;
-                        }
-                    }
-                    catch(OverflowException)
-                    {
-                        sw = File.AppendText("overflow.txt");
-                        sw.WriteLine(i);
-                        sw.Close();
-                    }
+                    int ab = a * b;
+                    sum += ab;
+                    count++;
                 }
-                catch
-                {
-                    sw = File.AppendText("bad_data.txt");
-                    sw.WriteLine(i);
-                    sw.Close();
-                }
+            }
+            catch (OverflowException)
+            {
+                sw = File.AppendText("overflow.txt");
+                sw.WriteLine(i);
+                sw.Close();
             }
             catch
             {
@@ -63,20 +52,28 @@ class Exceptions
                 sw.WriteLine(i);
                 sw.Close();
             }
+            
+
         }
+
+
         try
         {
             double avg = sum/ count;
             Console.WriteLine(avg);
+
         }
         catch (Exception e)
         {
             Console.WriteLine(e);
         }
-        static void Pictures()
-        {
-            
-            string[] files = Directory.GetFiles(Directory.GetCurrentDirectory());
+
+
+    }
+    public static void Pictures()
+    {
+        
+        string[] files = Directory.GetFiles(Directory.GetCurrentDirectory());
             Bitmap image;
             Regex searchPattern = new Regex("^((bmp)|(gif)|(tiff?)|(jpe?g)|(png))$", RegexOptions.IgnoreCase);
             foreach (var file in files)
@@ -85,7 +82,7 @@ class Exceptions
                 {
                     image = new Bitmap(file);
                     image.RotateFlip(RotateFlipType.RotateNoneFlipX);
-                    image.Save($"{Path.GetFileName(file)}-mirrored.gif");
+                    image.Save($"{Path.GetFileNameWithoutExtension(file)}-mirrored.gif",ImageFormat.Gif);
                 }
                 catch
                 {
@@ -96,10 +93,13 @@ class Exceptions
 
                 }
             }
-        }
+        
+        
     }
     static void Main()
     {
-        FileProcessing();
+       // FileProcessing();
+        Console.WriteLine("The attempt was a success");
+        Pictures();
     }
 }
